13a41a5c41169ab412a17145fc08f3531ef1a78c
Jennifer Tieu <jennifer.tieuu@gmail.com>
2023-02-13 22:52:47 -0600

    update  menu module

--- src/menu.js
+++ src/menu.js
@@ -1,62 +1,103 @@
 export default function menu(){
-    // create DOM elements
+    // create menu section 
     const menuElem = document.createElement("section");
     menuElem.setAttribute("id", "menu");
 
-    // heading
-    const heading = document.createElement("h1");
-    heading.textContent = "Menu";
-    menuElem.appendChild(heading);
+    // set menu heading
+    const menuHeading = document.createElement("h1");
+    menuHeading.textContent = "Menu";
+    menuElem.appendChild(menuHeading);
     
+    // create menu container for menu items 
     const menuContainer = document.createElement("div");
 
-    const rollsMenuItems = document.createElement("ul");
-    rollsMenuItems.classList.add("menu-items");
-    menuContainer.appendChild(rollsMenuItems);
+    function createMenuSection(title, items){
+        const menuSection = document.createElement("div");
+        
+        const sectionHeading = document.createElement("h2");
+        sectionHeading.textContent = title;
+        menuSection.appendChild(sectionHeading);
+        
+        const menuItems = document.createElement("ul");
+        menuItems.classList.add("menu-items");
+        items.forEach((elem) => menuItems.appendChild(elem))
 
-    const sashimiMenuItems = document.createElement("ul");
-    sashimiMenuItems.classList.add("menu-items");
-    menuContainer.appendChild(sashimiMenuItems);
+        menuSection.appendChild(menuItems);
 
-    // sushi name and ingredients
-    const signatureRolls = {
-        "Signature Yummy Roll": { ingredients: "spicy tuna, salmon, garlic chili oil, ponzu", price: "18"},
-        "Sunshine": { ingredients: "spicy tuna, mango, avocado, wasabi vinaigrette", price: "16"},
-        "Rainbow Dragon": { ingredients: "salmon, tuna, eel, avacado, cucumber", price: "19"},
-        "Delicious Roll": { ingredients: "yellowtail, avocado, tuna, truffle vinaigrette", price: "17"},
-    };
-
-    const sashimi = {
-        "Salmon": "6",
-        "Tuna": "5",
-        "Yellowtail": "5",
-        "Scallop": "4",
-    };
-
-    // iterate through sushi
-    for (const roll of signatureRolls){
-        // create element
-        let listElem = document.createElement("li");
-        let rollName = document.createElement("div");
-        rollName.classList.add("roll-name");
-        rollName.textContent = roll;
-
-        let rollPrice = document.createElement("span");
-        rollPrice.classList.add("roll-price");
-        rollPrice.textContent = signatureRolls[roll].price;
-        rollName.appendChild(rollPrice)
-
-        let rollIngredients = document.createElement("p");
-        rollIngredients.classList.add("roll-ingredients");
-        rollIngredients.textContent = signatureRolls[roll].ingredients;
-
-        listElem.appendChild(rollName);
-        listElem.appendChild(rollIngredients);
-
+        return menuSection;
     }
-    // add to menu
+
+    // roll menu items
+    const rollMenuSection = createMenuSection("Signature Rolls", createRollMenuItems());
+    // sashimi menu items
+    const sashimiMenuSection = createMenuSection("Sashimi", createSashimiMenuItems()); 
+
+    menuContainer.appendChild(rollMenuSection);
+    menuContainer.appendChild(sashimiMenuSection);
     
-    menuElem.appendChild(menuItems);
+    function createRollMenuItems(){
+        const menuItems = [];
+        // sushi name and ingredients
+        const signatureRolls = {
+            "Signature Yummy Roll": { ingredients: "spicy tuna, salmon, garlic chili oil, ponzu", price: "18"},
+            "Sunshine": { ingredients: "spicy tuna, mango, avocado, wasabi vinaigrette", price: "16"},
+            "Rainbow Dragon": { ingredients: "salmon, tuna, eel, avacado, cucumber", price: "19"},
+            "Delicious Roll": { ingredients: "yellowtail, avocado, tuna, truffle vinaigrette", price: "17"},
+        };
+        // iterate through sushi
+        for (const roll in signatureRolls){
+            let listElem = document.createElement("li");
+            let name = document.createElement("div");
+            name.classList.add("menu-name");
+            name.textContent = roll;
+
+            let price = document.createElement("span");
+            price.classList.add("menu-price");
+            price.textContent = signatureRolls[roll].price;
+            name.appendChild(price)
+
+            let ingredients = document.createElement("p");
+            ingredients.classList.add("menu-ingredients");
+            ingredients.textContent = signatureRolls[roll].ingredients;
+
+            listElem.appendChild(name);
+            listElem.appendChild(ingredients);
+
+            menuItems.push(listElem);
+        }
+
+        return menuItems;
+    }
+
+    function createSashimiMenuItems(){
+        const menuItems = [];
+        const sashimi = {
+            "Salmon": "6",
+            "Tuna": "5",
+            "Yellowtail": "5",
+            "Scallop": "4",
+        };
+
+        for (const item in sashimi){
+            let listElem = document.createElement("li");
+            let name = document.createElement("div");
+            name.classList.add("menu-name");
+            name.textContent = item;
+
+            let price = document.createElement("span");
+            price.classList.add("menu-price");
+            price.textContent = sashimi[item];
+            name.appendChild(price)
+
+            listElem.appendChild(name);
+
+            menuItems.push(listElem);
+        }
+
+        return menuItems;
+    }
     
+    menuElem.appendChild(menuContainer);
+
     return menuElem;
 }
